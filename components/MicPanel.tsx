7adb284e3d84743212203e2339d6fbcb9fa976a3
Fiona Haria <hariafiona@gmail.com>
2024-08-13 23:22:35 +0530

    msg with command

--- components/MicPanel.tsx
+++ components/MicPanel.tsx
@@ -11,6 +11,13 @@ type MicPanelProps = {
 
 const MicPanel: React.FC<MicPanelProps> = ({ handleVideoClick, onBulbClick }) => {
   const [isClient, setIsClient] = useState(false);
+  const [message, setMessage] = useState('');
+  const commands = [
+    {
+      command: 'Hi, I’m really frustrated. My delivery was supposed to arrive yesterday',
+      callback: () => setMessage('Hello Alex! I’m sorry to hear about the delay. Let me check the status of your delivery immediately')
+    },
+  ];
 
   useEffect(() => {
     // This ensures the code is only run on the client side
@@ -22,7 +29,7 @@ const MicPanel: React.FC<MicPanelProps> = ({ handleVideoClick, onBulbClick }) =>
     listening,
     resetTranscript,
     browserSupportsSpeechRecognition
-  } = useSpeechRecognition();
+  } = useSpeechRecognition({ commands });
 
   const [showSpeechToText, setShowSpeechToText] = useState(false);
   const [isListening, setIsListening] = useState(false);
@@ -36,11 +43,11 @@ const MicPanel: React.FC<MicPanelProps> = ({ handleVideoClick, onBulbClick }) =>
     if (isListening) {
       setShowSpeechToText(true);
       SpeechRecognition.startListening({ continuous: true, language: 'en-IN' });
-      console.log('Started listening')
+      console.log('Started listening');
     } else {
       setShowSpeechToText(false);
       SpeechRecognition.stopListening();
-      console.log('Stopped listening')
+      console.log('Stopped listening');
     }
     resetTranscript();
   }, [isListening]);
@@ -61,6 +68,7 @@ const MicPanel: React.FC<MicPanelProps> = ({ handleVideoClick, onBulbClick }) =>
   return (
     <div className="mt-40">
       {showSpeechToText && <p>{transcript}</p>}
+      {message && <p className="message">{message}</p>}
       <div className="flex flex-row justify-center items-center space-x-24">
         <button
           className={`flex items-center space-x-2 py-2 px-4 rounded-full border cursor-pointer transition-colors duration-300 
